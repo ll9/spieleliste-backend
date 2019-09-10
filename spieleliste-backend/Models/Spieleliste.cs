ccaf292bbbcaf6c2712d04c289320f5c4582915f
ll9 <llwierer@web.de>
2019-09-10 22:31:17 +0200

    init

---
+++ spieleliste-backend/Models/Spieleliste.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spieleliste_backend.Models
+{
+    public class Spieleliste
+    {
+        public ICollection<int> SpielIds { get; set; }
+    }
+}
