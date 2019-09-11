cff520d0cfd8930a79fcc37131a56a0aa54fb806
ll9 <llwierer@web.de>
2019-09-11 20:57:00 +0200

    unit of work

--- spieleliste-backend/Data/ApplicationDbContext.cs
+++ spieleliste-backend/Data/ApplicationDbContext.cs
@@ -1,16 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using spieleliste_backend.Data.Configurations;
 using spieleliste_backend.Models;
+using System.Threading.Tasks;
 
 namespace spieleliste_backend.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : DbContext, IUnitOfWork
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
 
         public DbSet<ListenEintrag> ListenEintraege { get; set; }
+
+        public Task Complete()
+        {
+            return SaveChangesAsync();
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
---
+++ spieleliste-backend/Data/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using spieleliste_backend.Models;
+
+namespace spieleliste_backend.Data
+{
+    public interface IUnitOfWork
+    {
+        DbSet<ListenEintrag> ListenEintraege { get; set; }
+
+        Task Complete();
+    }
+}
