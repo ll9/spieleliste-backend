3360cb9f818fec796d9cb11a1c10cd5400c0f5c5
ll9 <llwierer@web.de>
2019-09-11 20:51:32 +0200

    created test project

---
+++ spieleliste-backend-tests/Controllers/ListenEintragControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using NUnit.Framework;
+using spieleliste_backend.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spielelistebackendtests.Controllers
+{
+    public class ListenEintragControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
