0e157026e8fe8ef7ff20eb0c70955f8ca76d0cee
ll9 <llwierer@web.de>
2019-09-14 15:12:27 +0200

    declared users controller

---
+++ spieleliste-backend/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using spieleliste_backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spieleliste_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> List()
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> Create()
+        {
+            return null;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Remove()
+        {
+            return null;
+        }
+    }
+}
