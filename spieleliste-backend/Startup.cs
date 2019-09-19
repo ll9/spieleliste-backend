7d048d030cce6707e2870a8c6f45e54cc33658c7
ll9 <llwierer@web.de>
2019-09-19 21:53:12 +0200

    json circular reference settings

--- spieleliste-backend/Startup.cs
+++ spieleliste-backend/Startup.cs
@@ -23,7 +23,11 @@ namespace spieleliste_backend
             services.AddDbContext<ApplicationDbContext>(conf => conf.UseSqlite("Data Source = db.sqlite"));
             services.AddTransient<IUnitOfWork, UnitOfWork>();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(options => {
+                    options.SerializerSettings.ReferenceLoopHandling =
+                        Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
