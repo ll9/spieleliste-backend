aa8059983057518c942d4d6109fb4b63e0a6112c
ll9 <llwierer@web.de>
2019-09-24 20:22:02 +0200

    declared archive controller

---
+++ spieleliste-backend/Controllers/ArchiveController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using spieleliste_backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spieleliste_backend.Controllers
+{
+    [Route("api/archive")]
+    [ApiController]
+    public class ArchiveController: ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ArchiveEntry>>> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ spieleliste-backend/Models/ArchiveEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spieleliste_backend.Models
+{
+    public class ArchiveEntry
+    {
+        public int Id { get; set; }
+        public int IgdbId { get; set; }
+        public DateTime Archived { get; set; }
+    }
+}
