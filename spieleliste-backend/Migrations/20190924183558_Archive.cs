e133b3dda5d0f02365e78ce85c3c20308441867c
ll9 <llwierer@web.de>
2019-09-24 20:36:24 +0200

    archive migration

---
+++ spieleliste-backend/Migrations/20190924183558_Archive.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace spieleliste_backend.Migrations
+{
+    public partial class Archive : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ArchiveEntries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    IgdbId = table.Column<int>(nullable: false),
+                    Archived = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArchiveEntries", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ArchiveEntries");
+        }
+    }
+}
